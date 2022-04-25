6c519a22db6bb043838866278c64d3a326352100
Ken <ken.hanner@gmail.com>
2022-04-25 00:27:29 -0500

    add ArticleList.js and ArticleItem.js

---
+++ project-2-hanner/components/ArticleItem.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import articleStyles from '../styles/Article.module.css'
+
+const ArticleItem = ({article}) => {
+  return (
+    <Link href="/article/[id]">
+    
+    </Link>
+  )
+}
+
+export default ArticleItem
