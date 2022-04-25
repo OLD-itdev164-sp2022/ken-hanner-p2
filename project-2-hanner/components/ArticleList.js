8353f5bb16ea6b66c529ca328517fdf72a47ace8
Ken <ken.hanner@gmail.com>
2022-04-25 00:31:38 -0500

    create linked boxes with retreived data

--- project-2-hanner/components/ArticleList.js
+++ project-2-hanner/components/ArticleList.js
@@ -1,9 +1,12 @@
+import ArticleItem from './ArticleItem'
 import articleStyles from '../styles/Article.module.css'
 
 const ArticleList = ({articles}) => {
   return (
     <div className={articleStyles.grid}>
-        {articles.map(article=> <h3>{article.title}</h3>)}
+        {articles.map(article=> (
+            <ArticleItem article={article}/>
+        ))}
     </div>
   )
 }
