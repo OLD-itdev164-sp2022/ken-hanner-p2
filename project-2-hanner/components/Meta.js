b0358a801828f618ad13a981b12ec0077cf6f2c1
Ken <ken.hanner@gmail.com>
2022-04-25 01:34:14 -0500

    insert meta data

--- project-2-hanner/components/Meta.js
+++ project-2-hanner/components/Meta.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+
 const Meta = ({title, keywords, description}) => {
   return (
     <Head>
@@ -11,6 +12,12 @@ const Meta = ({title, keywords, description}) => {
         <title>{title}</title>
     </Head>
   )
+}
+
+Meta.defaultProps = {
+    title: 'WebDev News',
+    keywords: 'web development, programming',
+    description: 'Get the latest news in web dev!'
 }
 
 export default Meta
