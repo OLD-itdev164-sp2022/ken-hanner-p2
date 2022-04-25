9e149c3ad435113081c00b38f9143358316de3e8
Ken <ken.hanner@gmail.com>
2022-04-25 00:39:48 -0500

    get serverside props execution

--- project-2-hanner/pages/article/[id]/index.js
+++ project-2-hanner/pages/article/[id]/index.js
@@ -1,12 +1,30 @@
+import Link from 'next/Link'
 import {useRouter} from 'next/router'
 
 
-const article = () => {
+const article = ({article}) => {
     const router = useRouter()
     const {id} = router.query
 
 
-    return <div>this is article {id}</div>
+    return <>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br />
+        <Link href='/'>Go Back</Link>
+    </>
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
 }
 
 export default article
