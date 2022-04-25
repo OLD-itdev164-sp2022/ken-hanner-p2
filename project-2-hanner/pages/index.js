d0067d4a07cbb5e9a40a795a615689f8411ef34b
Ken <ken.hanner@gmail.com>
2022-04-25 00:20:46 -0500

    getStaticProps

--- project-2-hanner/pages/index.js
+++ project-2-hanner/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
@@ -11,4 +11,15 @@ export default function Home() {
       <h1>Welcome to Next</h1> 
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
